7d853789140a7ae93f9a78b30f2b900dfdde7094
Ivan <ivan.sch13@gmail.com>
2022-08-05 09:48:56 +0300

    нахождение сигмы из 1000 рандомных значений 1-100

---
+++ sample001/Program.cs
@@ -0,0 +1,35 @@
+﻿
+//double m = Convert.ToDouble();
+
+int mass=1000;
+int[] RS =new int [mass];
+double mean=0;
+double sum=0;
+double stddev=0;
+double stddev2=0;
+
+int i=0;
+
+for(i=0; i<mass;i++)
+    {
+    Random rnd = new Random();
+        RS [i] = rnd.Next(100);
+        //RS [i] = rnd.Next(49,51);
+    sum=sum+RS[i];
+    }
+    mean=sum/mass;
+for(i=0; i<mass;i++)
+    {
+    stddev2=stddev2+(RS[i]-mean)*(RS[i]-mean);
+    }
+stddev=Math.Sqrt(stddev2);
+
+
+
+Console.WriteLine(sum);
+Console.WriteLine(mean);
+Console.WriteLine(stddev2);
+Console.WriteLine(stddev);
+
+
+
